d09839e505551a294a0d4ce3479d2c37653f18c1
Usama <miansamusama@gmail.com>
2022-12-02 06:28:23 +0500

    feat(auth): implemented signin and signup logics

---
+++ instructions.md
@@ -0,0 +1,14 @@
+1. Create nest.js project using "nest new project-name"
+2. Add a module using "nest g/generate module module-name"
+3. Add a controller using "nest g/generate controller controller-name"
+
+
+PRISMA COMMANDS
+0. Add prisma client in the project
+1. Add db string in .env
+2. Add prisma using "npx prisma init"
+3. Add Schema in schema.prisma
+4. Create a db using MongoDB or MySQL
+5. If using SQL then run create dev migrations using "npx prisma migrate dev"
+6. Create a prisma module in src using command on "#2" in above para and connect db
+7. You can run prisma studio using "npx prisma stuio"
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import * as argon from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  signup() {
-    return { msg: 'I have signed up' };
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    try {
+      // generate the password
+      const hash = await argon.hash(dto.password);
+      // save the new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials already taken');
+        }
+      }
+      throw error;
+    }
   }
-  signin() {
-    return { msg: 'I have signed in' };
+  async signin(dto: AuthDto) {
+    // find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    // if user does not exist throw exception
+    if (!user) {
+      throw new ForbiddenException('User does not exist');
+    }
+    // compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    // if password incorrect throw exception
+    if (!pwMatches) {
+      throw new ForbiddenException('Invalid password');
+    }
+    // send back the user
+    delete user.hash;
+    return user;
   }
 }
