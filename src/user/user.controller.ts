091998af84eba23aa764e4ac0cec53d253ab8607
Usama <miansamusama@gmail.com>
2022-12-03 02:26:40 +0500

    feat(edit-user): added controller, service, and tests

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User as UserType } from '@prisma/client';
-import { User } from 'src/auth/decorator';
-import { JwtGuard } from 'src/auth/guard';
+import { User } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private readonly usersService: UserService) {}
+
   @Get('me')
   getMe(@User() user: UserType) {
     return user;
+  }
+
+  @Patch()
+  update(@User('id') userId: number, @Body() dto: EditUserDto) {
+    return this.usersService.editUser(userId, dto);
   }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,108 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { AppModule } from '../src/app.module';
+import { AuthDto } from 'src/auth/dto';
+import { EditUserDto } from 'src/user/dto';
 
-describe('AppController (e2e)', () => {
+describe('App e2e', () => {
   let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  let prisma: PrismaService;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+    await app.listen(3333);
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(() => {
+    app.close();
+  });
+
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'samtufail@gmail.com',
+      password: '123456',
+    };
+    describe('Signup', () => {
+      it('shoud throw error if email is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({ password: dto.password })
+          .expectStatus(400);
+      });
+      it('shoud throw error if password is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({ email: dto.email })
+          .expectStatus(400);
+      });
+      it('shoud throw error if body is empty', () => {
+        return pactum.spec().post('/auth/signup').expectStatus(400);
+      });
+      it('shoud signup', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+
+    describe('Signin', () => {
+      it('shoud signin', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200)
+          .stores('access_token', 'access_token');
+      });
+    });
+  });
+
+  describe('User', () => {
+    const dto: EditUserDto = {
+      email: 'samtufail@gmail.com',
+      firstName: 'Usama Tufail',
+      lastName: 'Muhammad Tufail',
+    };
+    describe('Get Me', () => {
+      it('should get current user', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({ Authorization: `Bearer $S{access_token}` })
+          .expectStatus(200);
+      });
+    });
+
+    describe('Edit User', () => {
+      it('should edit current user', () => {
+        return pactum
+          .spec()
+          .patch('/users')
+          .withBody(dto)
+          .withHeaders({ Authorization: `Bearer $S{access_token}` })
+          .expectStatus(200);
+      });
+    });
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create Bookmark', () => {});
+    describe('Get Bookmarks', () => {});
+    describe('Get Bookmark by Id', () => {});
+    describe('Edit Bookmark by Id', () => {});
+    describe('Delete Bookmark by Id', () => {});
   });
 });
